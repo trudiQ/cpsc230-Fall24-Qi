1b57022c2e936c5940de9e18f868e06e6961444f
Trudi Qi <agivenchy@hotmail.com>
2024-09-11 16:13:26 -0700

    added solution to in-class for Lecture 4 and HW2

---
+++ Assignments/HW_2.py
@@ -0,0 +1,53 @@
+# Homework for Lectures 3 & 4
+# Rename the file as HW_2_YOURNAME.py
+
+'''
+1. Write some code that asks the user for 5 different numbers, and stores
+them in the variables x1, x2, x3, x4, and x5. Use a set to create a
+unique set of numbers entered (e.g. if they enter 5 twice, only count it once.)
+Print out the set.
+'''
+# your code here
+
+
+
+'''
+2. Create a dictionary variable named student_1 to store the name, age, and major, of a random Chapman Student.
+'''
+# your code here
+
+
+
+'''
+3 Without using Python, figure out the answer to this statement.
+    Explain the order of operations IN YOUR YOUR REPORT.
+    You DONOT NEED to rewrite the expression using more paratheses. 
+
+    2 * 4 + 2 ** 3 // 2 + 6 - 7 * 9 + (16 + 4)
+
+Check your answer using Python. 
+YOU DON'T NEED TO WRITE CODE FOR THIS QUESTION.
+'''
+
+
+
+
+'''
+4. Using these operators (%,/,//,*), write some code to convert an amount of
+time in minutes (e.g. 257 minuts) into hours and minutes (e.g. 4 hours and 17 minutes).
+
+Hint: Starting from getting input minutes from the user using statement
+    e.g., mins_input = int(input("Please input some minutes: ")). 
+    Your code should then convert mins_input into two variables, X and Y, 
+    reprsenting hours and minutes respectively, 
+    by using operators %, /, //, *.
+
+Print out the numbers of hours and minutes in the format of "X hours, Y minutes".
+
+Verify your code using the following values:
+- 12938 (answer: 215 hours, 38 minutes)
+- 55 (answer: 0 hours, 55 minutes)
+- 525,600 (answer: 8760 hours, 0 minutes )
+- 432 (answer: 7 hours, 12 minutes)
+'''
+# your code here
